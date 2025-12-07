8d315bd6b0591cb89c34aee1d4e4c7fed9cd3c0c
Tony Aldridge <zaragopha@hotmail.com>
2025-12-07 18:33:18 +0000

    Add command line parser for picking which day we're working on

--- src/advent2025.adb
+++ src/advent2025.adb
@@ -1,6 +1,29 @@
+with Parse_Args; use Parse_Args;
 with Day_1;
 
 procedure Advent2025 is
+   AP : Argument_Parser;
 begin
-   Day_1.Run;
+   AP.Add_Option (
+      Make_Boolean_Option (False),
+      "help",
+      'h',
+      Usage => "Display this help text"
+      );
+   AP.Append_Positional (
+      Make_Integer_Option (Default => 1, Min => 1, Max => 1), "day"
+      );
+
+   AP.Parse_Command_Line;
+
+   if AP.Parse_Success and then AP.Boolean_Value ("help") then
+      AP.Usage;
+   elsif AP.Parse_Success then
+      case AP.Integer_Value ("day") is
+         when 1 => Day_1.Run;
+         when others => AP.Usage;
+      end case;
+   else
+      AP.Usage;
+   end if;
 end Advent2025;

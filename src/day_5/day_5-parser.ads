869beda6d360e69063bf40e82b5c215617be2add
Tony Aldridge <zaragopha@hotmail.com>
2025-12-14 13:08:32 +0000

    Use generics to simplify parsing

--- src/day_5/day_5-parser.ads
+++ src/day_5/day_5-parser.ads
@@ -2,6 +2,13 @@ with Types; use Types;
 with Day_5.Databases; use Day_5.Databases;
 
 package Day_5.Parser is
-   function Parse_Database (V : String_Vectors.Vector) return Database;
-   function Load_Database (Path : String) return Database;
+   procedure Parse_Line (Collector : in out Database; Line : String);
+
+   package P is new Parsers (
+      T => Database,
+      Parse_Line => Parse_Line
+   );
+
+   function Parse (Raw : String_Vectors.Vector) return Database renames P.Parse;
+   function Read_Database (Path : String) return Database renames P.Load;
 end Day_5.Parser;
